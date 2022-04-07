c208cd89e87d715684128574a7369a85d79af587
anciuvaga <andrei.ciuvaga@endava.com>
2022-04-07 14:36:37 +0300

    Override equals method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
